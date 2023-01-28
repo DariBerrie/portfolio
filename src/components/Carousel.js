12a8a703798662b1bb15b993f0c35c1b6488eaa0
Dari Goldman <darigoldman@gmail.com>
2023-01-28 18:15:23 +0100

    fix: made each section a full page for responsiveness.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -6,12 +6,14 @@ const Carousel = ({project_sec, skill_sec, projects}) => {
 
   const settings = {
     className: 'center',
-    centerMode: true,
     dots: true,
     accessibility: true,
-    arrows: true,
-    centerPadding: '60px',
+    arrows: false,
+    infinite: false,
+    initialSlide: 1,
     slidesToShow: 2,
+    slidesToScroll: 1,
+    centerPadding: '40px',
     speed: 500
   }
 
@@ -24,23 +26,21 @@ const Carousel = ({project_sec, skill_sec, projects}) => {
   }
 
   return (
-    <div className="row" ref={project_sec}>
-      <div id="projects-container">
-        <h2 id="projects-header" className= "mb-3 text-center">PROJECTS</h2>
+    <div id="projects-container" className="row" ref={project_sec}>
+      <h2 id="projects-header" className= "mb-3 text-center">PROJECTS</h2>
 
-        <p className="fs-5 mb-5">After spending two years studying computer science and web development, I'm ready to see my ideas on the screen.
-        Take a look at my recent projects! Many are primarily coded with the Ruby on Rails framework, but I'm currently getting comfortable
-        with Node.js and React.</p>
+      <p className="text-center fs-5 mb-5">After spending two years studying computer science and web development, I'm ready to see my ideas on the screen.
+      Take a look at my recent projects! Many are primarily coded with the Ruby on Rails framework, but I'm currently getting comfortable
+      with Node.js and React.</p>
 
-        <Slider {...settings}>
-          {projects.reverse().map(project =>
-            <Project key={project.id} project={project} /> )}
-        </Slider>
+      <Slider {...settings}>
+        {projects.reverse().map(project =>
+          <Project key={project.id} project={project} /> )}
+      </Slider>
 
-        <Image src="down-arrow.png" alt="Down Arrow to Projects" style={{width: "3em", cursor: "pointer", marginTop: "4em"}}
-          className="animate__animated animate__bounce animate__delay-5s align-self-center"
-          onClick={() => scrollToSection(skill_sec)} />
-    </div>
+      <Image src="down-arrow.png" alt="Down Arrow to Projects" style={{width: "4.5em", cursor: "pointer", marginTop: "10em"}}
+        className="animate__animated animate__bounce animate__delay-5s align-self-center"
+        onClick={() => scrollToSection(skill_sec)} />
   </div>
   )
 }

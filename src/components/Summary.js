5085a1cff6c50d0c7be739c7d891cf57e722a130
dariberrie <darigoldman@gmail.com>
2023-05-19 15:16:33 +0200

    fix: added link to summary

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -4,7 +4,8 @@ const Summary = () => {
       <p>I’m a <b>curious learner</b> driven to find aesthetic, seamless <b>solutions</b> whether that be
         in the code or on “the field” with colleagues and clients.
         If you’re looking to tell engaging stories with your product, <b>I’m your co-author</b>.</p>
-      <p>Hailing from Atlanta, but currently based in France, when I'm not working on new projects, I'm walking my dog,
+      <p>Hailing from Atlanta, but currently based in France, when I'm not tinkering on my own projects or contributing to 
+        <a href="https://www.github.com/rubyforgood">open source</a>, I'm walking my dog,
         cycling around the city, knitting a new sweater, or enjoying a nice meal with friends.<br/>
         Want to work together? Do you just have a question about web development?
         <a href="mailto:hello@dari.codes?subject=Hi, let's work together!"> I'd love to hear from you.</a></p>

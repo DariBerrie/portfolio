c515f94b2eef0220dd95026899466c1a1283fe68
dariberrie <darigoldman@gmail.com>
2023-05-20 10:58:55 +0200

    fix: updated Skils

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,11 +5,11 @@ const Skills = () => {
     <div className="card tech-skills">
       <h2>TECHNICAL</h2>
       <h5>BACK-END</h5>
-      <p>Ruby | Ruby on Rails | PostgreSQL | Node.js</p>
+      <p>Ruby | Ruby on Rails | PostgreSQL | Node.js | Java | Python</p>
       <h5>FRONT-END</h5>
-      <p>HTML5 | CSS3 | SCSS | Javascript | AJAX | Bootstrap | Tailwind CSS | ReactJS</p>
+      <p>HTML5 | CSS3 | SCSS | Javascript | AJAX | Bootstrap | Tailwind CSS | ReactJS | VueJS</p>
       <h5>OTHER</h5>
-      <p>Git | Heroku | Figma | Adobe CS | Googling</p>
+      <p>Git | Github | Heroku | Figma | Adobe CS | Googling</p>
     </div>
   )
 }
